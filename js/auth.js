874114fbdeceaec660702edfc2afcbffe3acb061
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-04-30 15:12:03 +0200

    Added auth page WIP
    still not finised with it

---
+++ js/auth.js
@@ -0,0 +1,7 @@
+var formSignIn = document.getElementById('signin');
+var formRegister = document.getElementById('register');
+
+function toggleForm() {
+    formSignIn.classList.toggle('d-none');
+    formRegister.classList.toggle('d-none');
+}
